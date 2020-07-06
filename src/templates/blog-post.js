cdf912d6aa09bc4a4ddab47d34dea53ec437040e
nachiketbhuta <nachiketbhuta@gmail.com>
2020-07-06 14:59:47 +0530

    Removed pagination

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -33,7 +33,7 @@ const BlogPostTemplate = ({ data, pageContext, location }) => {
               ...scale(-1 / 5),
               display: `block`,
               marginBottom: rhythm(1),
-              marginTop: `1%`
+              marginTop: `1%`,
             }}
           >
             {post.frontmatter.date} by {post.frontmatter.author}
@@ -46,33 +46,6 @@ const BlogPostTemplate = ({ data, pageContext, location }) => {
           }}
         />
       </article>
-
-      <nav>
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
-      </nav>
     </Layout>
   )
 }
